d105b11cb310980820c6ab3e127dd8d5c85b4cf1
Emmanuel Lyimo <esal29t@gmail.com>
2022-05-14 05:52:35 -0700

    updated the index.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,13 @@ const Navbar = () => {
             <h1>The Big Five</h1>
             <div className="links">
                <a href="/">Home</a>
-               {/* <a href="/"> About</a> */}
-               {/* <a href="/"> Contact Us</a> */}
+               <a href="/">About</a>
+               <a href="/">Contact Us</a>
+               <a href="/create" style={{ 
+                    color: 'white', 
+                    backgroundColor: 'green',
+                    borderRadius: '8px' 
+                    }}>World National Parks</a>
             </div>
         </nav>
     );
