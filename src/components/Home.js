d53b93b4b6190d33fd073593c548f168dcfea13b
Emmanuel Lyimo <esal29t@gmail.com>
2022-05-17 02:22:16 -0700

    updated the Navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+
 const Home = () => {
+
+const [name,setName] = useState('Ngorongoro');
+const [number,setNumber] = useState(1)
+    const handleClick = ()=> {
+        console.log('Hakuna, matata');
+        setName('The Serengeti')
+        setNumber(2)
+    }
+
+    // const handleClickAgain = (name, e) => {
+    //     console.log('Hakuna ' +name, e.target);
+    //   }
     return (
         <div className="home">
-            <h1>Homepage</h1>
+            <h2>Homepage</h2>
+            <p>{ name } is { number } National Park in the World!!</p>
+            <button onClick={handleClick}>Click me</button>
+            {/* <button onClick={(e) => handleClickAgain('matata',e)}>Click me Again</button> */}
         </div>
     );
 }
